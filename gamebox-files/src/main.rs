fccb4f4472b7eafc4eedbd7072e4383064d8ecde
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-10-29 20:04:39 +0100

    add a multi ghost file for testing

--- gamebox-files/src/main.rs
+++ gamebox-files/src/main.rs
@@ -1,6 +1,7 @@
 use gamebox_files::try_parse_buffer;
 
 fn main() {
-    let file = std::fs::read(env!("CARGO_MANIFEST_DIR").to_string() + "./test_replay.Gbx").unwrap();
+    let file =
+        std::fs::read(env!("CARGO_MANIFEST_DIR").to_string() + "./test_replay_multi.Gbx").unwrap();
     println!("{:?}", try_parse_buffer(file));
 }
