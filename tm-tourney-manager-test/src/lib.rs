ae963c8dfdc57396690c844015f94cf885dd1810
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-11-22 15:43:59 +0100

    insanely cursed test runner somewhat working

--- tm-tourney-manager-test/src/lib.rs
+++ tm-tourney-manager-test/src/lib.rs
@@ -1,89 +1,30 @@
-#![allow(dead_code, unused_imports)]
+#[cfg(test)]
+use tm_tourney_manager_api_rs::*;
 
-use spacetimedb_sdk::db_context::DbContext;
-use testcontainers::{Image, core::ContainerState, runners::SyncRunner};
-use tm_tourney_manager_api_rs::{DbConnection, TournamentTableAccess, create_tournament};
+#[cfg(test)]
 const PROJECT_NAME: &str = "tm-tourney-manager";
+
+#[cfg(test)]
+mod setup;
 
 #[test]
 #[should_panic]
 fn enforce_unique_tournament_name() {
-    use testcontainers::{
-        GenericImage, ImageExt,
-        core::{IntoContainerPort, WaitFor},
-    };
+    let db = setup::test();
 
-    let dir = env!("CARGO_MANIFEST_DIR").rsplit_once(r#"\"#).unwrap().0;
-
-    let path = "file://".to_string() + dir + r#"\"# + PROJECT_NAME;
-
-    _ = std::process::Command::new("cargo")
-        .args([
-            "build",
-            "--package",
-            &path,
-            "--target",
-            "wasm32-unknown-unknown",
-        ])
-        .output()
-        .expect("failed to build your spacetime server module.");
-
-    let spacetime = SyncRunner::start(
-        GenericImage::new("clockworklabs/spacetime", "v1.9.0")
-            .with_exposed_port(3000.tcp())
-            .with_wait_for(WaitFor::message_on_stdout(
-                "spacetimedb-standalone version: 1.9.0",
-            ))
-            .with_cmd(["start"]),
-    )
-    .unwrap();
-
-    let spacetime_url = format!(
-        "http://{}:{}",
-        spacetime.get_host().unwrap(),
-        spacetime.get_host_port_ipv4(3000).unwrap()
-    );
-
-    let published_module_name: String = "howdoideterminethisname".into();
-
-    let published_dir = dir.to_string()
-        + "/target/wasm32-unknown-unknown/debug/"
-        + &PROJECT_NAME.replace("-", "_")
-        + ".wasm";
-
-    _ = std::process::Command::new("spacetime")
-        .args([
-            "publish",
-            "-b",
-            &published_dir,
-            "-s",
-            &spacetime_url,
-            &published_module_name,
-        ])
-        .output()
-        .expect("failed to build your spacetime server module.");
-
-    let spacetime = DbConnection::builder()
-        .with_module_name(published_module_name)
-        .with_uri(spacetime_url)
-        .build()
-        .expect("Failed to connect to SpacetimeDB. Aborting.");
-
-    spacetime.reducers.on_create_tournament(|t, _| {
-        if let spacetimedb_sdk::Status::Failed(event) = &t.event.status {
+    db.reducers.on_create_tournament(|ctx, _| {
+        if let spacetimedb_sdk::Status::Failed(event) = &ctx.event.status {
             panic!("{event}")
         }
     });
 
-    _ = spacetime
+    _ = db
         .reducers
         .create_tournament("there shouldn't be duplicate names".into());
 
-    _ = spacetime
+    _ = db
         .reducers
         .create_tournament("there shouldn't be duplicate names".into());
 
-    loop {
-        spacetime.advance_one_message().unwrap();
-    }
+    db.run();
 }
---
+++ tm-tourney-manager-test/src/setup.rs
@@ -0,0 +1,98 @@
+use tm_tourney_manager_api_rs::DbConnection;
+
+pub(crate) fn test() -> DatabaseInterface {
+    use testcontainers::{
+        GenericImage, ImageExt,
+        core::{IntoContainerPort, WaitFor},
+        runners::SyncRunner,
+    };
+
+    let dir = env!("CARGO_MANIFEST_DIR").rsplit_once(r#"\"#).unwrap().0;
+
+    let path = "file://".to_string() + dir + r#"\"# + super::PROJECT_NAME;
+
+    std::process::Command::new("cargo")
+        .args([
+            "build",
+            "--package",
+            &path,
+            "--target",
+            "wasm32-unknown-unknown",
+        ])
+        .output()
+        .expect("failed to build your spacetime server module.");
+
+    let _spacetime_container = SyncRunner::start(
+        GenericImage::new("clockworklabs/spacetime", "v1.9.0")
+            .with_exposed_port(3000.tcp())
+            .with_wait_for(WaitFor::message_on_stdout(
+                "spacetimedb-standalone version: 1.9.0",
+            ))
+            .with_cmd(["start"]),
+    )
+    .unwrap();
+
+    let spacetime_url = format!(
+        "http://{}:{}",
+        _spacetime_container.get_host().unwrap(),
+        _spacetime_container.get_host_port_ipv4(3000).unwrap()
+    );
+
+    let published_module_name: String = "howdoideterminethisname".into();
+
+    let published_dir = dir.to_string()
+        + "/target/wasm32-unknown-unknown/debug/"
+        + &super::PROJECT_NAME.replace("-", "_")
+        + ".wasm";
+
+    std::process::Command::new("spacetime")
+        .args([
+            "publish",
+            "-b",
+            &published_dir,
+            "-s",
+            &spacetime_url,
+            &published_module_name,
+        ])
+        .output()
+        .expect("failed to build your spacetime server module.");
+
+    let spacetime = DbConnection::builder()
+        .with_module_name(published_module_name)
+        .with_uri(spacetime_url)
+        .build()
+        .expect("Failed to connect to SpacetimeDB. Aborting.");
+
+    DatabaseInterface {
+        _spacetime_container,
+        spacetime,
+        messages: std::cell::RefCell::new(0),
+    }
+}
+
+pub(crate) struct DatabaseInterface {
+    _spacetime_container: testcontainers::Container<testcontainers::GenericImage>,
+    spacetime: DbConnection,
+    messages: std::cell::RefCell<u32>,
+}
+
+impl DatabaseInterface {
+    pub(crate) fn run(&self) {
+        println!("{}", *self.messages.borrow());
+        let mut msg = 0;
+        while msg < *self.messages.borrow() {
+            self.spacetime.advance_one_message_blocking().unwrap();
+            msg += 1;
+        }
+    }
+}
+
+impl std::ops::Deref for DatabaseInterface {
+    type Target = DbConnection;
+
+    fn deref(&self) -> &Self::Target {
+        // The 5 is an arbitrary value which sounds good to my ears :shrug:.
+        *self.messages.borrow_mut() += 5;
+        &self.spacetime
+    }
+}
