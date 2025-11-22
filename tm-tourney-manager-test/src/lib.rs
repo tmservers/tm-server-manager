401ed271e7327daa2986f7b514f9b3965b83ebb7
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-11-22 15:03:48 +0100

    remove even more async stuff from test

--- tm-tourney-manager-test/src/lib.rs
+++ tm-tourney-manager-test/src/lib.rs
@@ -1,11 +1,8 @@
 #![allow(dead_code, unused_imports)]
-use std::sync::OnceLock;
 
 use spacetimedb_sdk::db_context::DbContext;
 use testcontainers::{Image, core::ContainerState, runners::SyncRunner};
 use tm_tourney_manager_api_rs::{DbConnection, TournamentTableAccess, create_tournament};
-use tokio::runtime;
-
 const PROJECT_NAME: &str = "tm-tourney-manager";
 
 #[test]
@@ -14,7 +11,6 @@ fn enforce_unique_tournament_name() {
     use testcontainers::{
         GenericImage, ImageExt,
         core::{IntoContainerPort, WaitFor},
-        runners::AsyncRunner,
     };
 
     let dir = env!("CARGO_MANIFEST_DIR").rsplit_once(r#"\"#).unwrap().0;
@@ -67,17 +63,11 @@ fn enforce_unique_tournament_name() {
         .output()
         .expect("failed to build your spacetime server module.");
 
-    let con = DbConnection::builder()
+    let spacetime = DbConnection::builder()
         .with_module_name(published_module_name)
         .with_uri(spacetime_url)
         .build()
         .expect("Failed to connect to SpacetimeDB. Aborting.");
-
-    static SPACETIME: OnceLock<DbConnection> = OnceLock::new();
-
-    _ = SPACETIME.set(con);
-
-    let spacetime = SPACETIME.wait();
 
     spacetime.reducers.on_create_tournament(|t, _| {
         if let spacetimedb_sdk::Status::Failed(event) = &t.event.status {
@@ -87,13 +77,13 @@ fn enforce_unique_tournament_name() {
 
     _ = spacetime
         .reducers
-        .create_tournament("this is a banger tournament".into());
+        .create_tournament("there shouldn't be duplicate names".into());
 
     _ = spacetime
         .reducers
-        .create_tournament("this is a banger tournament".into());
+        .create_tournament("there shouldn't be duplicate names".into());
 
     loop {
-        SPACETIME.wait().advance_one_message().unwrap();
+        spacetime.advance_one_message().unwrap();
     }
 }
