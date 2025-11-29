d33f8f1bec48986559677e9b9c62990ea1d9711b
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-11-29 12:35:32 +0100

    aint no way working ts client bindings package

--- test_tourney_setup_commands.bat
+++ test_tourney_setup_commands.bat
@@ -1,5 +1,5 @@
 spacetime publish --break-clients -c -y -p tm-tourney-manager tm-tourney-manager
 spacetime generate --yes --lang rust --out-dir tm-tourney-manager-api-rs/src/generated --project-path tm-tourney-manager
-spacetime generate --yes --lang typescript --out-dir tm-tourney-manager-api-ts/src/gen --project-path tm-tourney-manager
+spacetime generate --yes --lang typescript --out-dir tm-tourney-manager-api-ts/src --project-path tm-tourney-manager
 spacetime call tm-tourney-manager create_tournament TestTourneyy
 spacetime call tm-tourney-manager create_match 1 1 null
