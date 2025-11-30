57c04f84e65763ea2c9d6af38c3246eb8f2b399e
MRegterschot <marijn.regterschot@gmail.com>
2025-11-30 15:19:19 +0100

    feat: added linux script

---
+++ test_tourney_setup_commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e  # stop on errors (except where overridden)
+
+# Try to delete DB if it exists (ignore errors if not found)
+spacetime delete tm-tourney-manager || true
+
+# Publish module as "tourney-manager"
+spacetime publish --break-clients -c -y -p tm-tourney-manager tm-tourney-manager
+
+# Generate Rust and TS client APIs
+spacetime generate --yes --lang rust --out-dir tm-tourney-manager-api-rs/src/generated --project-path tm-tourney-manager
+spacetime generate --yes --lang typescript --out-dir tm-tourney-manager-api-ts/src --project-path tm-tourney-manager
+
+spacetime call tm-tourney-manager create_tournament TestTourneyy
+spacetime call tm-tourney-manager create_match 1 1 null
