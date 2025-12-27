1d1c583e7a89c43636ba77e025bf4533dc4de367
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-12-27 11:08:39 +0100

    update bat to marign sh version

--- test_tourney_setup_commands.bat
+++ test_tourney_setup_commands.bat
@@ -1,7 +1,37 @@
 spacetime publish --break-clients --delete-data=on-conflict -y -p tm-tourney-manager tm-tourney-manager
 spacetime generate --yes --lang rust --out-dir tm-tourney-manager-api-rs/src/generated --project-path tm-tourney-manager
 spacetime generate --yes --lang typescript --out-dir tm-tourney-manager-api-ts/tourney-manager --project-path tm-tourney-manager
-spacetime call tm-tourney-manager create_tournament TestTourneyy
+
+spacetime publish --break-clients --delete-data=on-conflict -y -p tm-tourney-manager tm-tourney-manager
+
+spacetime generate --yes --lang rust --out-dir tm-tourney-manager-api-rs/src/generated --project-path tm-tourney-manager
+spacetime generate --yes --lang typescript --out-dir tm-tourney-manager-api-ts/tourney-manager --project-path tm-tourney-manager
+
+spacetime call tm-tourney-manager create_tournament "My Tournament"
+
 spacetime call tm-tourney-manager create_match 1 null
 spacetime call tm-tourney-manager create_match 1 null
-spacetime call tm-tourney-manager create_competition "whatever" 1 null
+
+spacetime call tm-tourney-manager create_competition "Division 1" 1 null
+
+spacetime call tm-tourney-manager create_competition "League Phase" 2 null
+spacetime call tm-tourney-manager create_match 3 null
+spacetime call tm-tourney-manager create_match 3 null
+spacetime call tm-tourney-manager create_match 3 null
+spacetime call tm-tourney-manager create_match 3 null
+
+spacetime call tm-tourney-manager create_competition "Playoffs" 2 null
+spacetime call tm-tourney-manager create_match 4 null
+spacetime call tm-tourney-manager create_match 4 null
+
+spacetime call tm-tourney-manager create_competition "Division 2" 1 null
+
+spacetime call tm-tourney-manager create_competition "League Phase" 5 null
+spacetime call tm-tourney-manager create_match 6 null
+spacetime call tm-tourney-manager create_match 6 null
+spacetime call tm-tourney-manager create_match 6 null
+spacetime call tm-tourney-manager create_match 6 null
+
+spacetime call tm-tourney-manager create_competition "Playoffs" 5 null
+spacetime call tm-tourney-manager create_match 7 null
+spacetime call tm-tourney-manager create_match 7 null
