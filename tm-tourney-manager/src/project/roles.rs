962b43990db84355bd00a6d8fbfee21b78753530
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2026-02-26 10:25:45 +0100

    start with first role reducer

--- tm-tourney-manager/src/project/roles.rs
+++ tm-tourney-manager/src/project/roles.rs
@@ -1,6 +1,6 @@
-use spacetimedb::{ReducerContext, Uuid, table};
+use spacetimedb::{ReducerContext, Uuid, reducer, table};
 
-use crate::project::permissions::ProjectPermissionsV1;
+use crate::{authorization::Authorization, project::permissions::ProjectPermissionsV1};
 
 #[table(accessor= tab_project_role)]
 pub struct ProjectRole {
@@ -13,12 +13,12 @@ pub struct ProjectRole {
     #[index(hash)]
     project_id: u32,
 
-    permissions1: u64,
+    permissions: u64,
 }
 
 impl ProjectRole {
     pub(crate) fn get_permissions1(&self) -> ProjectPermissionsV1 {
-        ProjectPermissionsV1(self.permissions1)
+        ProjectPermissionsV1(self.permissions)
     }
 }
 
@@ -28,11 +28,38 @@ pub struct ProjectRoleMember {
     role_id: u32,
 
     #[index(hash)]
-    account_id: Uuid,
+    account_id: u32,
 }
 
 impl ProjectRoleMember {
     pub fn get_role_id(&self) -> u32 {
         self.role_id
     }
+}
+
+#[table(accessor= tab_project_member,index(accessor= user_roles , hash(columns= [project_id,account_id])))]
+pub struct ProjectMember {
+    #[index(hash)]
+    project_id: u32,
+
+    #[index(hash)]
+    account_id: u32,
+
+    permissions: u64,
+}
+
+#[reducer]
+pub fn project_member_add(
+    ctx: &ReducerContext,
+    project_id: u32,
+    account_id: Uuid,
+) -> Result<(), String> {
+    let account_id = ctx.get_user_account()?;
+    ctx.auth_builder(project_id, account_id)?
+        //.permission(ProjectPermissionsV1::Pro)
+        .authorize()?;
+
+    //TODO
+
+    Ok(())
 }
